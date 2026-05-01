23343c37af91895a6f960584d1b4c9462ecc6648
Botnie Data <botniedata@gmail.com>
2026-05-01 18:23:32 +0800

    update files

--- datacamp-python/introduction_to_python/conditional_operators/for.py
+++ datacamp-python/introduction_to_python/conditional_operators/for.py
@@ -7,6 +7,6 @@ ingredients = [
 for item in ingredients:
     print(item)
 
-# Iterate ove the number of ingredients
+# Iterate over the number of ingredients
 for item in range(1, 7):
     print("Adding ingredient", item)
