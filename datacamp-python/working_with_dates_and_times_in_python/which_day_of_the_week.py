3dc259c8a4cf66fa81a2b489a821276d9f997c03
Botnie Data <botniedata@gmail.com>
2026-05-07 07:05:26 +0800

    new chapter date and calendars

---
+++ datacamp-python/working_with_dates_and_times_in_python/which_day_of_the_week.py
@@ -0,0 +1,8 @@
+# Import date from datetime (1)
+from datetime import date
+
+# Create a date object for August 24, 1992 (2)
+hurracane_andrew = date(1992, 8, 24)
+
+# Which day of the week is the date?
+print(hurracane_andrew.weekday()) # 0 - Monday
